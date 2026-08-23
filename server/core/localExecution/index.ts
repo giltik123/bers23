8cd1ee20904b71777be294b6e1546d1d10c60769
giltik123 <getknk@yandex.ru>
2026-08-24 09:02:18 +1000

    Sprint 6.42A: export local execution persistence

--- server/core/localExecution/index.ts
+++ server/core/localExecution/index.ts
@@ -1,2 +1,5 @@
 export * from './LocalExecutionAdmission.ts';
 export * from './LocalExecutionTicketAuthority.ts';
+export * from './PostgresLocalExecutionUploadStore.ts';
+export * from './localExecutionUploadSchema.ts';
+export * from './LocalSegmentationExecutionService.ts';
