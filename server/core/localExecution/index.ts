27760699243f96428ec3c443cdbf27b5ab8a4f56
giltik123 <getknk@yandex.ru>
2026-08-24 09:41:34 +1000

    Keep local execution barrel runtime-only

--- server/core/localExecution/index.ts
+++ server/core/localExecution/index.ts
@@ -1,5 +1,4 @@
 export * from './LocalExecutionAdmission.ts';
 export * from './LocalExecutionTicketAuthority.ts';
 export * from './PostgresLocalExecutionUploadStore.ts';
-export * from './localExecutionUploadSchema.ts';
 export * from './LocalSegmentationExecutionService.ts';
