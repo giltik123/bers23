0b69116b9b826145a1199f02be818c3372c789f6
giltik123 <getknk@yandex.ru>
2026-08-28 16:42:47 +1000

    Centralize Resize v1 numeric contract identity

--- src/platform/creative/deterministic/ResizeIdentity.js
+++ src/platform/creative/deterministic/ResizeIdentity.js
@@ -9,3 +9,9 @@ export const RESIZE_CAPABILITY = 'local:tool:resize:v1';
 export const RESIZE_OPERATION = 'RESIZE';
 /** @type {'resize'} */
 export const RESIZE_STEP_ID = 'resize';
+/** @type {16} */
+export const RESIZE_FIXED_POINT_BITS = 16;
+/** @type {16384} */
+export const RESIZE_MAX_DIMENSION = 16384;
+/** @type {16777216} */
+export const RESIZE_MAX_OUTPUT_PIXELS = 16777216;
